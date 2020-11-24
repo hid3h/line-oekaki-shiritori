d3925200e505f77851d742918002a6ef543d57ca
hidetaka hasegawa <>
2020-11-24 21:19:42 +0900

    画像アップ修正

--- backend/app/models/s3.rb
+++ backend/app/models/s3.rb
@@ -4,19 +4,32 @@ class S3
   IMAGE_UPLOAD_BUCKET = 'line-oekaki-images'
 
   def initialize
-    @s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
+    @client = Aws::S3::Client.new(region: 'ap-northeast-1')
   end
 
   def put(data)
+    file_name = Digest::SHA1.hexdigest(data)
+
     split = data.split(',')
     body = split[1]
+    content_type = get_content_type(split[0]) # "data:image/png;base64"
+    key = "img/" + file_name
 
-    file_name = Digest::SHA1.hexdigest(data)
-    bucket = @s3.bucket(IMAGE_UPLOAD_BUCKET)
-    obj = bucket.object('img/' + file_name)
-
-    content_type = 'image/png'
-    obj.put(body: Base64.decode64(body), content_type: content_type)
+    @client.put_object({
+      body: Base64.decode64(body), 
+      bucket: IMAGE_UPLOAD_BUCKET, 
+      key: key, 
+      content_type: content_type,
+      content_encoding: 'base64'
+    })
     file_name
   end
+
+  private
+
+  def get_content_type(str)
+    str2 = str.split(':')[1]
+    str3 = str2.split(';')[0]
+  end
+
 end
