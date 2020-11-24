52404e3ac29dadd7e6ba9a3d9472c0d6413b7e40
hidetaka hasegawa <>
2020-11-24 20:54:39 +0900

    contentypeとか

--- backend/app/models/s3.rb
+++ backend/app/models/s3.rb
@@ -1,7 +1,7 @@
 require 'aws-sdk-s3'
 
 class S3
-  IMAGE_UPLOAD_BUCKET = 'line-oekaki-images/img'
+  IMAGE_UPLOAD_BUCKET = 'line-oekaki-images'
 
   def initialize
     @s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
@@ -10,8 +10,10 @@ class S3
   def put(data)
     file_name = Digest::SHA1.hexdigest(data)
     bucket = @s3.bucket(IMAGE_UPLOAD_BUCKET)
-    obj = bucket.object(file_name)
-    obj.put(body: data)
+    obj = bucket.object('img/' + file_name)
+
+    content_type = 'image/png'
+    obj.put(body: data, content_type: content_type)
     file_name
   end
 end
