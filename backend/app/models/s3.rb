a7fb828700cbcc167a494ff92db67ccb23b7c8fc
hidetaka hasegawa <>
2020-11-24 21:09:16 +0900

    画像アップ修正

--- backend/app/models/s3.rb
+++ backend/app/models/s3.rb
@@ -8,12 +8,15 @@ class S3
   end
 
   def put(data)
+    split = data.split(',')
+    body = split[1]
+
     file_name = Digest::SHA1.hexdigest(data)
     bucket = @s3.bucket(IMAGE_UPLOAD_BUCKET)
     obj = bucket.object('img/' + file_name)
 
     content_type = 'image/png'
-    obj.put(body: data, content_type: content_type)
+    obj.put(body: Base64.decode64(body), content_type: content_type)
     file_name
   end
 end
