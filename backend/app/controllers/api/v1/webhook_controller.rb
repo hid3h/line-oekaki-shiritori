dfff1f7c91dfcbac37cd0df74a890c7afa075deb
hide <razyetts@hotmail.co.jp>
2020-12-27 09:49:56 +0900

    jets削除

--- backend/app/controllers/api/v1/webhook_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::WebhookController < ApplicationController
-  def receive
-    render :json => 'ok'
-  end
-
-  def test
-    render :json => {test: 'oekaki2'}
-  end
-end
--- backend/config/environments/development.rb
+++
@@ -1,8 +0,0 @@
-Jets.application.configure do
-  # Example:
-  # config.function.memory_size = 1536
-
-  # config.action_mailer.raise_delivery_errors = false
-  # Docs: http://rubyonjets.com/docs/email-sending/
-  config.cors = true
-end
