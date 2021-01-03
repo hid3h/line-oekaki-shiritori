f08c92bbd6d68e1b60de489fa9415ae5f6a2981d
hide <razyetts@hotmail.co.jp>
2021-01-03 21:10:50 +0900

    rootにしないとだめ？

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '', to: 'home#index'
+  root '', to: 'home#index'
   get 'health', to: 'application#health'
 end
