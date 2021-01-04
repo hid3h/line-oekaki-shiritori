23b8d169a555efedfd43c4f64b5d1313e75fe348
hide <razyetts@hotmail.co.jp>
2021-01-04 18:38:08 +0900

    tail

--- backend/frontend/config/webpack.common.js
+++ backend/frontend/config/webpack.common.js
@@ -40,7 +40,29 @@ module.exports = {
             ]
           }
         }
-      }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
     ]
   }
 };
--- backend/frontend/packs/app.js
+++ backend/frontend/packs/app.js
@@ -1,1 +1,2 @@
 import '../src/js/home/index'
+import "tailwindcss/tailwind.css"
