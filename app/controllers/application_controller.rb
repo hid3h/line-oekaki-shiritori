9707127bac4386b5eabc6d950252973694882f80
hide <razyetts@hotmail.co.jp>
2021-03-07 20:16:32 +0900

    どうやったら本番でset-cookieつくの？

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :create_session
+
   def health
     render :json => {"eshiritori": "ok"}
+  end
+
+  private
+
+  def create_session
+    !!session[:user_id]
   end
 end
