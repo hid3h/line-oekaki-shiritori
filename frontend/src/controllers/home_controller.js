997f60728fc03f54083a94519594e66de5e2865e
hide <razyetts@hotmail.co.jp>
2021-03-07 18:22:36 +0900

    strict-origin-when-cross-originかも

--- frontend/src/controllers/home_controller.js
+++ frontend/src/controllers/home_controller.js
@@ -44,14 +44,14 @@ export default class extends StimulusController {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'include', // include, *same-origin, omit
+      credentials: 'same-origin', // include, *same-origin, omit
       headers: {
         'Content-Type': 'application/json',
         // 'Content-Type': 'application/x-www-form-urlencoded',
         'X-CSRF-Token': this.csrf_token(),
       },
       redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      referrerPolicy: 'strict-origin-when-cross-origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data) // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
     })
     .then(response => response.json())
