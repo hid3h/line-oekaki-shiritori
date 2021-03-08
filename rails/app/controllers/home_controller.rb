2365c1bd6434af7e064dfc13fe710f4812ad36fe
hidetaka hasegawa <>
2021-03-08 19:15:14 +0900

    旧rails削除

--- rails/app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    p "aaaaa"
-  end
-end
--- rails/frontend/src/controllers/stimulus_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "stimulus"
-
-export default class StimulusController extends Controller {
-
-  csrf_token() {
-    return document.getElementsByName('csrf-token')[0].content
-  }
-}
